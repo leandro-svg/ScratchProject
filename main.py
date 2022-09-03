199ec0dde6f6af0dc42797f7f9866b9826231e14
leandro-svg <leandro.dibella@gmail.com>
2022-09-03 11:52:46 +0200

    Adding average

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import torch
 import torchvision
 import numpy
 
+
+def average(a,b):
+    c = (a+b)/2
+    return c
+
 if __name__ == '__main__':
     print(torch.__version__)
     print(torchvision.__version__)
     print(numpy.__version__)
 
+    a  = 2
+    b = 3
+    average_c = average(a,b) 
+    print(average_c)
 
     
