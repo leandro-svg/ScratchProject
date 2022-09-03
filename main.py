3a28a55d406be1bf88d85d81de2ff2c84e7c6eb2
leandro-svg <leandro.dibella@gmail.com>
2022-09-03 11:48:44 +0200

    Add numpy to main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import torch
 import torchvision
-
+import numpy
 
 if __name__ == '__main__':
     print(torch.__version__)
     print(torchvision.__version__)
+    print(numpy.__version__)
 
 
     
