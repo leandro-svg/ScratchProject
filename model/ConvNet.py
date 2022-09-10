6a4d598df94004fe9b512c1aa7a38b4f33486221
leandro-svg <leandro.dibella@gmail.com>
2022-09-10 15:00:41 +0200

    Testing

---
+++ model/ConvNet.py
@@ -0,0 +1,46 @@
+import torch
+import torchvision
+import numpy
+from torch.nn import Conv2d, Module, Linear, MaxPool2d, ReLU, LogSoftmax
+from torch import flatten
+
+class ConvNet(Module):
+    def __init__(self, num_channels, classes):
+        super(ConvNet, self).__init__()
+        
+        self.conv1 = Conv2d(in_channels=num_channels, out_channels=20, 
+            kernel_size=(5,5))
+        
+        self.relu1 = ReLU()
+        self.Maxpool1 = MaxPool2d(kernel_size=(2,2), stride=(2,2))
+
+        self.conv2 = Conv2d(in_channels=20, out_channels=50, 
+            kernel_size=(5,5))
+        
+        self.relu2 = ReLU()
+        self.Maxpool2 = MaxPool2d(kernel_size=(2,2), stride=(2,2))
+
+        self.fc1 = Linear(in_features=800, out_features=500)
+        self.relu3 = ReLU()
+
+        self.fc2 = Linear(in_features=500, out_features=classes)
+        self.LogSoftmax = LogSoftmax(dim=1)
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.Maxpool1(x)
+
+        x = self.conv2(x)
+        x=  self.relu2(x)
+        x = self.Maxpool2(x)
+
+        x = flatten(x, 1)
+        x = self.fc1(x)
+        x = self.relu3(x)
+
+        x = self.fc2(x)
+        output = self.LogSoftmax(x)
+
+        return output
--- test.py
+++ test.py
@@ -1,13 +1,66 @@
-import numpy
+import numpy as np
+np.random.seed(42)
+
+from torch.utils.data import DataLoader
+from torch.utils.data import Subset
+from torchvision.transforms import ToTensor
+from torchvision.datasets import KMNIST
+import argparse
+import imutils
+import cv2
+import torch
+
 class Test():
     def __init__(self):
         super().__init__()
-        self.test = True
-        print("We reached it")
 
-    def is_working(self):
-        print("Yeah it is.")
+
+
+    def getParser(self):
+        ap = argparse.ArgumentParser()
+        ap.add_argument("-m", "--model", type = str, required=True,
+        help = "path to pytorch model")
+        ap.add_argument("-o", "--output", type = str, required=True,
+        help = "path to saved results")
+        args = vars(ap.parse_args())
+        return args
 
 if __name__ == '__main__':
-    print(numpy.__version__)
+    
+    args = Test().getParser()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    filename = args["output"]
+    print("[INFO] loading the KMNIST test dataset...")
+    testData = KMNIST(root="data", train=False, download= True,
+    transform = ToTensor())
+    idxs = np.random.choice(range(0, len(testData)), size=(10,))
+    testData = Subset(testData, idxs)
 
+    testDataLoader = DataLoader(testData, batch_size = 1)
+
+    model = torch.load(args["model"])
+    model = model.to(device)
+    model.eval()
+
+    with torch.no_grad():
+        for (image, label) in testDataLoader:
+            origImage = image.numpy().squeeze(axis=(0,1))
+            gtLabel = testData.dataset.classes[label.numpy()[0]]
+
+            image = image.to(device)
+            pred = model(image)
+
+            idx = pred.argmax(axis=1).cpu().numpy()[0]
+            predLabel = testData.dataset.classes[idx]
+
+            origImage = np.dstack([origImage] * 3)
+            origImage = imutils.resize(origImage, width=128)
+            # draw the predicted class label on it
+            color = (0, 255, 0) if gtLabel == predLabel else (0, 0, 255)
+            image = cv2.putText(origImage, gtLabel, (2, 25),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.95, color, 2)
+            # display the result in terminal and show the input image
+            print("[INFO] ground truth label: {}, predicted label: {}".format(
+                gtLabel, predLabel))
+            cv2.imwrite(filename+str(label)+".jpg", image)
+            
