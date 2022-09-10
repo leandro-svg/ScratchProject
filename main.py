38abf854c5a1f8a0a8c3172653b7f9fe608f271f
leandro-svg <leandro.dibella@gmail.com>
2022-09-10 18:45:06 +0200

    Improve training

--- main.py
+++ main.py
@@ -3,58 +3,20 @@ import torchvision
 import numpy
 import sys
 from model.LeandDetect import LeandroDetect
-from LeandDetect_2 import LeandroDetect_2
-from test_class import Test
 
 class Visualizer():
     def __init__(self):
         super().__init__()
 
-        self.threshold = 0.5
-        self.use_cp = True
-        print("Itinitializer of Visualizer")
-    
-    def average(self, a, b):
-        c = (a+b)/2
-        return c
-    
-    def make_threshold(self, a):
-        if a > self.threshold:
-            a = a
-        else:
-            a = 0
-        return a
-
-
     
 
-def average(a,b):
-    c = (a+b)/2
-    return c
 
 if __name__ == '__main__':
     print(torch.__version__)
     print(torchvision.__version__)
     print(numpy.__version__)
 
-    a  = 2
-    b = 3
-    average_c = average(a,b) 
-    print(average_c)
-
-    visualizer = Visualizer()
-    c = visualizer.average(a,b)
-    new_c = visualizer.make_threshold(c)
-    print(new_c)
-
-    detector = LeandroDetect()
-    print("detector", detector)
-
-    test = Test()
-    test.is_working()
-    leandrodetect2  = LeandroDetect_2()
-
-    
+  
 
 
     
--- train.py
+++ train.py
@@ -17,146 +17,178 @@ import torch
 import time
 
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-m", 
-                "--model", 
-                type=str,
-                required=True, 
-                help="path to output trained model")
-ap.add_argument("-p",
-                "--plot", 
-                type=str, 
-                required=True, 
-                help="path to output loss/accuracy plot")
-ap.add_argument("--init_lr", type=int, default=1e-3)
-ap.add_argument("--BATCH_SIZE", type=int, default=64)
-ap.add_argument("--EPOCH", type=int, default=10)
-ap.add_argument("--TRAIN_SPLIT", type=int, default=0.75)
-ap.add_argument("--VAL_SPLIT", type=int, default=0.25)
-ap.add_argument("--get_device", default="cuda")
-args = ap.parse_args()
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+class Trainer():
+    def __init__(self, args):
+        super().__init__()
+        print("Training begins...")
 
-print("[INFO] loading the KMINST data...")
-trainData = KMNIST(root="data", train=True, download=True, transform=ToTensor())
-testData = KMNIST(root="data", train=False, download=True, transform=ToTensor())
+    def DataLoading(self):
+        print("[INFO] loading the KMINST data...")
+        self.trainData = KMNIST(root="data", train=True, download=True, transform=ToTensor())
+        self.testData = KMNIST(root="data", train=False, download=True, transform=ToTensor())
 
-print("[INFO] generating the train/validatiion split...")
+        print("[INFO] generating the train/validatiion split...")
 
-numTrainSamples = int(len(trainData)*args.TRAIN_SPLIT)
-numValSamples = int(len(trainData)*args.VAL_SPLIT)
-(trainData, valData) = random_split(trainData, 
-                        [numTrainSamples, numValSamples],
-                        generator = torch.Generator().manual_seed(42))
+        numTrainSamples = int(len(self.trainData)*args.TRAIN_SPLIT)
+        numValSamples = int(len(self.trainData)*args.VAL_SPLIT)
+        (self.trainData, valData) = random_split(self.trainData, 
+                                [numTrainSamples, numValSamples],
+                                generator = torch.Generator().manual_seed(42))
 
-trainDataLoader = DataLoader(trainData, shuffle=True, batch_size = args.BATCH_SIZE)
-valDataLoader = DataLoader(valData, shuffle=False, batch_size = args.BATCH_SIZE)
-testDataLoader = DataLoader(testData, shuffle=False, batch_size = args.BATCH_SIZE)
+        self.trainDataLoader = DataLoader(self.trainData, shuffle=True, batch_size = args.BATCH_SIZE)
+        self.valDataLoader = DataLoader(valData, shuffle=False, batch_size = args.BATCH_SIZE)
+        self.testDataLoader = DataLoader(self.testData, shuffle=False, batch_size = args.BATCH_SIZE)
 
 
-trainSteps = len(trainDataLoader.dataset) // args.BATCH_SIZE
-valSteps = len(valDataLoader.dataset) // args.BATCH_SIZE
+        self.trainSteps = len(self.trainDataLoader.dataset) // args.BATCH_SIZE
+        self.valSteps = len(self.valDataLoader.dataset) // args.BATCH_SIZE
+        
+        self.H = {
+            "train_loss" : [],
+            "train_acc" : [],
+            "val_loss" : [],
+            "val_acc" : []
+        }
+        
+
+    def modelLoader(self):
+        print("[INFO] Initializing the ConvNet model")
+
+        self.model = ConvNet(num_channels=1, classes=len(self.trainData.dataset.classes)).to(device)
+
+        self.opt = Adam(self.model.parameters(), lr=args.init_lr)
+        self.lossFn = nn.NLLLoss()
+
+    def training(self):
+
+        print("[INFO] training our neural network...")
+        startTime = time.time()
+
+        for e in range(0, args.EPOCH):
+            self.model.train()
+
+            totalTrainLoss = 0
+            totalValLoss = 0
+
+            trainCorrect = 0
+            valCorrect = 0
+
+            for (x,y) in self.trainDataLoader:
+                (x,y) = (x.to(device), y.to(device))
+
+                pred =  self.model(x)
+                loss = self.lossFn(pred, y)
+
+                self.opt.zero_grad()
+                loss.backward()
+                self.opt.step()
+
+                totalTrainLoss += loss
+                trainCorrect += (pred.argmax(1) == y).type(
+                    torch.float).sum().item()
+            with torch.no_grad():
+                self.model.eval()
+
+                for (x,y) in self.valDataLoader:
+                    (x,y) = (x.to(device), y.to(device))
+
+                    pred = self.model(x)
+                    totalValLoss += self.lossFn(pred, y)
+
+                    valCorrect += (pred.argmax(1) == y).type(
+                        torch.float).sum().item()
+
+            avgTrainLoss = totalTrainLoss / self.trainSteps
+            avgValLoss = totalValLoss / self.valSteps
+
+            trainCorrect = trainCorrect/ len(self.trainDataLoader.dataset)
+            valCorrect = valCorrect  /len(self.valDataLoader.dataset)
+
+            self.H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
+            self.H["train_acc"].append(trainCorrect)
+            self.H["val_loss"].append(avgValLoss.cpu().detach().numpy())
+            self.H["val_loss"].append(valCorrect)
+
+            print("[INFO] EPOCH: {}/{}".format(e + 1, args.EPOCH))
+            print("Train loss : {:.6f}, Train accuracay : {:.4f}".format(
+                avgTrainLoss, trainCorrect))
+            print("Val loss : {:.6f}, Val accuracay : {:.4f}".format(
+                avgValLoss, valCorrect))
+            
+
+        endTime = time.time()
+        print("[INFO] total time taken to trian the model : {:.2f}s".format(
+        endTime - startTime))
+
+    def eval(self):
+        print("[INFO] evaluation network")
+
+        with torch.no_grad():
+            self.model.eval()
+
+            preds = []
+            for (x,y) in self.testDataLoader:
+                x = x.to(device)
+
+                pred = self.model(x)
+                preds.extend(pred.argmax(axis=1).cpu().numpy())
+
+        print(classification_report(self.testData.targets.cpu().numpy(),
+        np.array(preds), target_names= self.testData.classes))
+
+    def plot(self):
+        print("[INFO] Saving plot")
+
+        plt.style.use("ggplot")
+        plt.figure()
+        plt.plot(self.H["train_loss"], label="train_loss")
+        plt.plot(self.H["val_loss"], label="val_loss")
+        plt.plot(self.H["train_acc"], label="train_acc")
+        plt.plot(self.H["val_acc"], label="val_acc")
+        plt.title("Training Loss and Accuracy on Dataset")
+        plt.xlabel("Epoch #")
+        plt.ylabel("Loss/Accuracy")
+        plt.legend(loc="lower left")
+        plt.savefig(args.plot)
+
+    def save_model(self):
+        print("[INFO] Saving model")
+
+        torch.save(self.model, args.model)
+
+def get_parser():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-m", 
+                    "--model", 
+                    type=str,
+                    required=True, 
+                    help="path to output trained model")
+    ap.add_argument("-p",
+                    "--plot", 
+                    type=str, 
+                    required=True, 
+                    help="path to output loss/accuracy plot")
+    ap.add_argument("--init_lr", type=int, default=1e-3)
+    ap.add_argument("--BATCH_SIZE", type=int, default=64)
+    ap.add_argument("--EPOCH", type=int, default=10)
+    ap.add_argument("--TRAIN_SPLIT", type=int, default=0.75)
+    ap.add_argument("--VAL_SPLIT", type=int, default=0.25)
+    ap.add_argument("--get_device", default="cuda")
+    ap.add_argument("--use_plot", action="store_true")
+    args = ap.parse_args()
+    return args
 
 
-print("[INFO] Initializing the LeanNet model")
-
-model = ConvNet(num_channels=1, classes=len(trainData.dataset.classes)).to(device)
-
-opt = Adam(model.parameters(), lr=args.init_lr)
-lossFn = nn.NLLLoss()
-
-H = {
-    "train_loss" : [],
-    "train_acc" : [],
-    "val_loss" : [],
-    "val_acc" : []
-}
-
-print("[INFO] training our neural network...")
-startTime = time.time()
-
-for e in range(0, args.EPOCH):
-    model.train()
-
-    totalTrainLoss = 0
-    totalValLoss = 0
-
-    trainCorrect = 0
-    valCorrect = 0
-
-    for (x,y) in trainDataLoader:
-        (x,y) = (x.to(device), y.to(device))
-
-        pred =  model(x)
-        loss = lossFn(pred, y)
-
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
-
-        totalTrainLoss += loss
-        trainCorrect += (pred.argmax(1) == y).type(
-            torch.float).sum().item()
-    with torch.no_grad():
-        model.eval()
-
-        for (x,y) in valDataLoader:
-            (x,y) = (x.to(device), y.to(device))
-
-            pred = model(x)
-            totalValLoss += lossFn(pred, y)
-
-            valCorrect += (pred.argmax(1) == y).type(
-                torch.float).sum().item()
-
-    avgTrainLoss = totalTrainLoss / trainSteps
-    avgValLoss = totalValLoss / valSteps
-
-    trainCorrect = trainCorrect/ len(trainDataLoader.dataset)
-    valCorrect = valCorrect  /len(valDataLoader.dataset)
-
-    H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
-    H["train_acc"].append(trainCorrect)
-    H["val_loss"].append(avgValLoss.cpu().detach().numpy())
-    H["val_loss"].append(valCorrect)
-
-    print("[INFO] EPOCH: {}/{}".format(e + 1, args.EPOCH))
-    print("Train loss : {:.6f}, Train accuracay : {:.4f}".format(
-        avgTrainLoss, trainCorrect))
-    print("Val loss : {:.6f}, Val accuracay : {:.4f}".format(
-        avgValLoss, valCorrect))
+if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    args = get_parser()
+    trainer = Trainer(args)
+    trainer.DataLoading()
+    trainer.modelLoader()
+    trainer.training()
+    trainer.eval()
+    if args.use_plot:
+        trainer.plot()
+    trainer.save_model()
     
-
-endTime = time.time()
-print("[INFO] total time taken to trian the model : {:.2f}s".format(
-endTime - startTime))
-
-print("[INFO] evaluation network")
-
-with torch.no_grad():
-    model.eval()
-
-    preds = []
-    for (x,y) in testDataLoader:
-        x = x.to(device)
-
-        pred = model(x)
-        preds.extend(pred.argmax(axis=1).cpu().numpy())
-
-print(classification_report(testData.targets.cpu().numpy(),
-np.array(preds), target_names= testData.classes))
-
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(H["train_loss"], label="train_loss")
-plt.plot(H["val_loss"], label="val_loss")
-plt.plot(H["train_acc"], label="train_acc")
-plt.plot(H["val_acc"], label="val_acc")
-plt.title("Training Loss and Accuracy on Dataset")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend(loc="lower left")
-plt.savefig(args.plot)
-
-torch.save(model, args.model)
